89ee840c976f25f8ca3927b9b08c59f141da6ee2
Fedyk Nazar Krystian <107977176+KrystianFedykProgrammer@users.noreply.github.com>
2022-08-10 10:22:35 +0200

    Lesson3

--- main.py
+++ main.py
@@ -1,84 +1,191 @@
 '''
 Task 1
-
-The greeting program.
-
-Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
-
-     "Good day <name>! <day> is a perfect day to learn some python."
-Note that  <name> and <day> are predefined variables in source code.
-
-An additional bonus will be to use different string formatting methods for constructing result string.
+String manipulation
+Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
+If the string length is less than 2, return instead of the empty string.
+Sample String: 'helloworld'
+Expected Result : 'held'
+Sample String: 'my'
+Expected Result : 'mymy'
+Sample String: 'x'
+Expected Result: Empty String
+Tips:
+Use built-in function len() on an input string
+Use positive indexing to get the first characters of a string and negative indexing to get the last characters
 '''
 
-my_name = 'Nazar'
-weekday = 'Monday'
-print(f'Good day {my_name}! {weekday} is a perfect day to learn some python.'+'\n')
+word = 'helloworld'
+
+if len(word) >= 2:
+    print(f'{word[0:2]}{word[-2:]}\n')
+else:
+    print()
 
 '''
 Task 2
-
-Manipulate strings.
-
-Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+The valid phone number program.
+Make a program that checks if a string is in the right format for a phone number. 
+The program should check that the string contains only numerical characters and is only 10 characters long. 
+Print a suitable message depending on the outcome of the string evaluation.
 '''
 
 
-surname = 'Fedyk'
-print(surname + ' ' + my_name + '\n')
-print(f"Hello world! My name is {surname} {my_name}! And now I'm learning Python programming" + '\n')
+cell_number = input('Введіть будь ласка ваш номер: ')
+OPERATOR_CODE = [55, 67, 99, 97, 66]
+count = False
+i = 0
+
+while i < len(OPERATOR_CODE):
+    if len(cell_number) == 10 and cell_number.isdigit() and int(cell_number[1:3]) == OPERATOR_CODE[i]:
+        count = True
+        break
+    i += 1
+
+if count:
+    print('Your number format is correct')
+else:
+    print('Your number format is incorrect')
+
+print()
 
 '''
 Task 3
-
-Using python as a calculator.
-
-Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following: 
-
-Addition
-Subtraction
-Division
-Multiplication
-Exponent (Power)
-Modulus
-Floor division
+The math quiz program.
+Write a program that asks the answer for a mathematical expression, checks whether the user 
+is right or wrong, and then responds with a message accordingly.
 '''
 
-a = 4
-b = 2
+'''
+Перша версія
 
-print(f'The result of addition is a number: {a+b}')
-print(f'The result of subtraction is a number: {a-b}')
-print(f'The result of division is a number: {a/b}')
-print(f'The result of multiplication is a number: {a*b}')
-print(f'The result of exponent (Power) is a number: {a**b}')
-print(f'The result of modulus is a number: {a%b}')
-print(f'The result of subtraction is a number: {a//b}' + '\n')
+print('Скільки буде 15 + 15: ')
+a = int(input())
+
+if a == 30:
+    print('Ваша відповідь правильна!')
+elif 25 < a < 30:
+    print('Ваша відповідь не правильна, але ви були близько.')
+else:
+    print('Нажаль ви вказали не правильну відповідь!')'''
+
+print('Привіт, давай зіграємо в гру. Я задаю тобі питання, а ти відповідаєш на них!')
+#print('Щоб пропустити вікторону введіть 00!')
+QUESTION = ['Скільки буде 15 + 15: ', 'Скільки буде 100 + 100: ', 'Скільки буде 150 + 150: ']
+ANSWERS = [30, 200, 300]
+i = 0
+
+'''Ось тут додав список відповідей, і вказав його в циклі, для того щоб виключити імовірність того, 
+що користувач введе одинакові значення на всі запитання'''
+
+while i < len(QUESTION):
+    print(QUESTION[i])
+    a = int(input())
+
+    if a == ANSWERS[i]:
+        print('Ваша відповідь правильна!', end='\n\n')
+    else:
+        '''if a == 00:
+            break'''
+        print('Нажаль ви вказали не правильну відповідь!', end='\n\n')
+    i += 1
+
+print()
 
 '''
-Task 4 
+Task 4
+The name check.
+Write a program that has a variable with your name stored (in lowercase) and then asks for your name as input. 
+The program should check if your input is equal to the stored name even if the given name has another case, e.g., 
+if your input is “Anton” and the stored name is “anton”, it should return True.
+'''
 
-Під зірочкой
-на форматування. Зробіть форматування так щоб отримати ось таке
-"000012 Василий 110110 32.10"
-заповніть ___ , майте на увазі 110110 - це бінарний формат.
-print("____________________".format(12, "Василий", 54, 32.1))
+my_name = 'nazar'
+input_name = input("Введіть ваше ім'я: ")
+
+print(input_name.lower() == my_name)
+
+print()
+
+'''
+Вхідні дані для всіх завдань можна отримувати двома способами: функція input(), або аргументи програми sys.argv
+Task1
+Write a Python program to construct the following pattern, using a while loop
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+Плюс за можливість динамічно змінити максимальну кількість зірочок в одному рядку (в прикладі 5) '''
+
+SYMBOL = '*'
+row = 5
+i = 1
+
+while i < row:
+    print("* " * i)
+    i += 1
+
+i = row
+
+while i >= 1:
+    print("* " * i)
+    i -= 1
+
+print()
+
+'''
+Task2
+Write a python program, which sums all digits in a python string.
+Examples, input - ‘1234’, output - 10
 '''
 
 
+num = input('Введіть будь ласка будь-яке число: ')
+final_number = 0
+i = 0
 
-print("{0:0>6} {1} {2:b} {3:5.2f}".format(12, "Василий", 54, 32.1)+'\n')
+while i < len(num):
+    if num[i].isalpha():
+        print('Ви ввели не числове значення!') #Додав перевірку для нечислового значення
+        final_number = 0
+        break
+    else:
+        final_number += int(num[i])
+    i += 1
+if final_number > 0:
+    print(f'Сума всіх цифер вашого числа: {final_number}')
 
-
+print()
 
 '''
-Task 5
-Спробуйте "зібрати" зі слова "Корован" слово "ворона". Використайте слайсінг та вашу фантазію.
-s1 = "Корован"
-print(s1[4]+s1[1]+s1[2] +... <-  можлиіий але не самий цікавий варіант.
+Task3
+Write a Python program that accepts a string and calculate the number of digits, 
+letters and other characters in the input string
 '''
 
-main_word = "Корован"
-word = main_word[-3:-7:-1] + main_word[-1:-3:-1]
-word = word.capitalize()
-print(word)
+counter_digit = 0
+counter_letter = 0
+counter_sign = 0
+sentence = input('Будь ласка введіть будь що забажаєте: ')
+i = 0
+
+while i < len(sentence):
+    if sentence[i].isdigit():
+        counter_digit += 1
+    elif sentence[i].isalpha():
+        counter_letter += 1
+    elif sentence[i].isspace():
+        i += 1
+        continue
+    else:
+        counter_sign += 1
+    i += 1
+
+print(f'Кількість введених цифер:{counter_digit}')
+print(f'Кількість введених літер:{counter_letter}')
+print(f'Кількість введених знаків:{counter_sign}')
+
